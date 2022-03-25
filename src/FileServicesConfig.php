804db6eae7e6b6d61a4b0a324a1bb71daa536160
dzaki236 <dzakiahnafz@gmail.com>
2022-03-26 00:13:03 +0700

    add error handling

--- src/FileServicesConfig.php
+++ src/FileServicesConfig.php
@@ -1,10 +1,16 @@
 <?php 
 namespace Dzaki236\LoggingServices;
+
+use Exception;
+
 class FileServicesConfig extends FilesPatching{
    protected $file;
    public function __construct($filepath) {
-      // $this->var = $var;
       $this->file = "$filepath";
+      if (!file_exists(public_path("$this->file"))) {
+         # code...
+         throw new Exception("File not found".public_path("$this->file")." check again at spelling file,or check at your path / config path");
+      }
    }
    
 }
