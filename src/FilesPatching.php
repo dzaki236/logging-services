d0e578b822c625d4d164ed272073c451dbbcebcd
dzaki236 <dzakiahnafz@gmail.com>
2022-04-04 13:10:27 +0700

    field insert secured feature (will be release later)

--- src/FilesPatching.php
+++ src/FilesPatching.php
@@ -1,6 +1,8 @@
 <?php
 namespace Dzaki236\LoggingServices;
 
+use Illuminate\Support\Facades\Storage;
+
 class FilesPatching
 {
    /**
@@ -56,5 +58,47 @@ class FilesPatching
       $files = fopen(public_path("$this->file"), 'a');
       fwrite($files, $format);
       fclose($files);
+   }
+
+
+   /**
+    * return @void
+    */
+   public function secureFieldInsert(array $fields, array $fill):void
+   {
+      # for insert by field (Secured) Maybe will release later)!
+      if (!Storage::exists($this->file)) {
+         # code...
+         throw new \Exception("File not found!,please run 'php artisan storage:link' to link your file, and create custom logging file on 'storage' folder on your laravel project");
+      }
+      $file_ = file(Storage::url("$this->file"), FILE_IGNORE_NEW_LINES);
+      if (!isset($file_[0])) {
+         $field_ = '';
+         foreach ($fields as $key => $field) {
+            $field_ .= '|' . $field;
+         }
+         $field_ = substr($field_, 1);
+         $files = fopen(Storage::url("$this->file"), 'a');
+         fwrite($files, $field_);
+         fclose($files);
+         $format_ = '';
+         foreach ($fill as $key => $fills) {
+            $format_ .= '|' . $fills;
+         }
+         $formats_ = "\n" . substr($format_, 1);
+         $files = fopen(Storage::url("$this->file"), 'a');
+         fwrite($files, $formats_);
+         fclose($files);
+      } else {
+         $format_ = '';
+         foreach ($fill as $key => $fills) {
+            # code...
+            $format_ .= '|' . $fills;
+         }
+         $formats_ = "\n" . substr($format_, 1);
+         $files = fopen(Storage::url("$this->file"), 'a');
+         fwrite($files, $formats_);
+         fclose($files);
+      }
    }
 }
